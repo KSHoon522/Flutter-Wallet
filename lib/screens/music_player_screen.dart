eb1dd89aade81f61491be9ec3c58b6ee9705263b
Sang Hoon Kim <KSHoon522@gamil.com>
2023-12-19 11:09:57 +0900

    Album PageView / AnimatedSwitcher

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -8,12 +10,101 @@ class MusicPlayerScreen extends StatefulWidget {
 }
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  final PageController _pageController = PageController(
+    viewportFraction: 0.8,
+  );
+  int _currentPage = 0;
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Music Player"),
-      ),
-    );
+        backgroundColor: Colors.black,
+        body: Stack(
+          children: [
+            AnimatedSwitcher(
+              duration: const Duration(
+                milliseconds: 500,
+              ),
+              child: Container(
+                key: ValueKey<int>(_currentPage),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "assets/covers/${_currentPage + 1}.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 10,
+                    sigmaY: 10,
+                  ),
+                  child: Container(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+            ),
+            PageView.builder(
+              onPageChanged: _onPageChanged,
+              controller: _pageController,
+              itemCount: 5,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 350,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                          )
+                        ],
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/covers/${index + 1}.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 35,
+                    ),
+                    const Text(
+                      'InterStellar',
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Hans Zimmer',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            )
+          ],
+        ));
   }
 }
