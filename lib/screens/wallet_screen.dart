a5970f4ed3f670defeb2284aeed335e339869ad7
Sang Hoon Kim <KSHoon522@gamil.com>
2023-12-26 14:10:59 +0900

    Credit card / AnimateList

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -17,28 +17,105 @@ class _WalletScreenState extends State<WalletScreen> {
       appBar: AppBar(
         title: const Text("Wallet"),
       ),
-      body: Center(
-        child: const Text(
-          "Hello!",
-          style: TextStyle(fontSize: 66),
-        )
-            .animate()
-            .fadeIn(
-              begin: 0,
-              duration: 3.seconds,
-            )
-            .scale(
-              alignment: Alignment.center,
-              begin: Offset.zero,
-              duration: 3.seconds,
-              end: const Offset(1, 1),
-            )
-            .then()
-            .slideX(
-              begin: 0,
-              end: -10,
-              duration: 2.seconds,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const CreditCard(bgColor: Colors.purple),
+            const CreditCard(bgColor: Colors.black),
+            const CreditCard(bgColor: Colors.blue),
+          ]
+              .animate(
+                interval: 500.milliseconds,
+              )
+              .fadeIn(
+                begin: 0,
+              )
+              .slideX(
+                begin: -1,
+                end: 0,
+              ),
+        ),
+      ),
+    );
+  }
+}
+
+class CreditCard extends StatelessWidget {
+  final Color bgColor;
+
+  const CreditCard({super.key, required this.bgColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: bgColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 40,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Sanggu",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      "**** **** **02",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 20,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
